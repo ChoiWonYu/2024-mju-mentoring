bbfe0bfe98f8d5c1685856f54d32fdcba60efcb1
WonyuChoi <phillip07290@naver.com>
2024-03-31 14:54:20 +0900

    feat: 미션 엔티티로 변경

--- src/main/java/com/mju/mentoring/mission/domain/mission/Mission.java
+++ src/main/java/com/mju/mentoring/mission/domain/mission/Mission.java
@@ -2,12 +2,50 @@ package com.mju.mentoring.mission.domain.mission;
 
 import com.mju.mentoring.global.domain.OperateType;
 import com.mju.mentoring.global.domain.ResourceType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+
+@Entity
+@Getter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Mission {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column
     private String title;
+
+    @Enumerated(EnumType.STRING)
     private ResourceType targetType;
+
+    @Enumerated(EnumType.STRING)
     private OperateType operateType;
+
+    @Column
     private Long goal;
+
+    @Column
     private Long reward;
+
+    public boolean hasSameInfo(final ResourceType targetType, final OperateType operateType) {
+        return this.targetType.equals(targetType) && this.operateType.equals(operateType);
+    }
 }
