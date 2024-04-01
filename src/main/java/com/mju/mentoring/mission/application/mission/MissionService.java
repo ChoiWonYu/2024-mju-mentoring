8fd57bfaa60aa73b210395c41a7449f4dd50673a
WonyuChoi <phillip07290@naver.com>
2024-04-01 15:10:29 +0900

    feat: 미션 이벤트 발송 기능 추가

--- src/main/java/com/mju/mentoring/mission/application/mission/MissionService.java
+++ src/main/java/com/mju/mentoring/mission/application/mission/MissionService.java
@@ -3,17 +3,19 @@ package com.mju.mentoring.mission.application.mission;
 import com.mju.mentoring.global.domain.OperateType;
 import com.mju.mentoring.global.domain.ResourceType;
 import com.mju.mentoring.global.event.Events;
+import com.mju.mentoring.mission.domain.mission.ChallengeMissionEvent;
 import com.mju.mentoring.mission.domain.mission.Mission;
-import com.mju.mentoring.mission.domain.mission.MissionFoundEvent;
+import com.mju.mentoring.mission.domain.mission.TargetOperateEvent;
 import com.mju.mentoring.mission.domain.mission.MissionRepository;
 import com.mju.mentoring.mission.exception.exceptions.NotFoundMissionException;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MissionService {
 
     private final MissionRepository missionRepository;
@@ -24,21 +26,17 @@ public class MissionService {
 
     public void findTargetMission(final Long challengerId, final OperateType operateType,
         final ResourceType resourceType) {
-        Optional<Mission> targetMission = missionRepository.findByOperateTypeAndResourceType(
-            operateType, resourceType);
-
-        if (targetMission.isEmpty()) {
-            return;
-        }
-
-        Mission mission = targetMission.get();
-        Events.raise(new MissionFoundEvent(mission.getId(), challengerId));
+        missionRepository.findByOperateTypeAndResourceType(operateType, resourceType)
+            .ifPresent(
+                mission -> Events.raise(new TargetOperateEvent(mission.getId(), challengerId)));
     }
 
     public void challengeMission(final Long challengerId, final Long missionId) {
-        Optional<Mission> mission = missionRepository.findById(missionId);
-        if (mission.isEmpty()) {
-            throw new NotFoundMissionException(missionId);
-        }
+        missionRepository.findById(missionId)
+            .ifPresentOrElse(mission -> Events.raise(
+                    new ChallengeMissionEvent(challengerId, missionId, mission.getGoal())),
+                () -> {
+                    throw new NotFoundMissionException(missionId);
+                });
     }
 }
--- src/main/java/com/mju/mentoring/mission/domain/mission/ChallengeMissionEvent.java
+++ src/main/java/com/mju/mentoring/mission/domain/mission/ChallengeMissionEvent.java
@@ -1,13 +1,15 @@
 package com.mju.mentoring.mission.domain.mission;
 
+
 import com.mju.mentoring.global.event.Event;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class MissionFoundEvent extends Event {
+public class ChallengeMissionEvent extends Event {
 
-    private final Long missionId;
     private final Long challengerId;
+    private final Long missionId;
+    private final Long goal;
 }
