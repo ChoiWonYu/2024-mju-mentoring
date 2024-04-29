cc8c234967f5c950002ad3e227e8d9532134adfc
WonyuChoi <phillip07290@naver.com>
2024-04-30 02:37:11 +0900

    feat: converter 예외 처리

--- src/main/java/com/mju/mentoring/mission/exception/MissionExceptionHandler.java
+++ src/main/java/com/mju/mentoring/mission/exception/MissionExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class MissionExceptionHandler {
@@ -15,8 +16,15 @@ public class MissionExceptionHandler {
         return getResponseWithStatus(HttpStatus.NOT_FOUND, exception);
     }
 
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<String> handleConversionFailedException(
+        final MethodArgumentTypeMismatchException exception
+    ) {
+        return getResponseWithStatus(HttpStatus.BAD_REQUEST, exception);
+    }
+
     private ResponseEntity<String> getResponseWithStatus(final HttpStatus httpStatus,
-        final NotFoundMissionException exception) {
+        final Exception exception) {
         return ResponseEntity.status(httpStatus)
             .body(exception.getMessage());
     }
